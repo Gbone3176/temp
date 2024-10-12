7384f2c242aa75b9e90a3208e9eee6fa1314d9a7
Gbone3176 <21307130160@m.fudan.edu.cn>
2024-10-12 16:50:27 +0800

    test

--- learn_absl.py
+++ learn_absl.py
@@ -6,7 +6,7 @@ from main import FLAGS
 def learn_absl(args):
     del args
     for i in range(FLAGS.nums):
-        print("hello! %s" % FLAGS.name)
+        print("readme.md! %s" % FLAGS.name)
 
 
 if __name__ == "__main__":
